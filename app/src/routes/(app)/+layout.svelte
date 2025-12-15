6e92ea05175cd771d451b5aff6c961ed93ff395f
rider <ridebus82@nate.com>
2025-12-15 11:50:32 +0900

    251215 asdlfjogogogo

--- app/src/routes/(app)/+layout.svelte
+++ app/src/routes/(app)/+layout.svelte
@@ -446,7 +446,7 @@
                     {/each}
                 </div>
 
-                {#if !bottomFixFormData.formAgree || bottomFixFormData.formAgree == "use"}
+                {#if bottomFixFormData.formAgree || bottomFixFormData.formAgree == "use"}
                     <div class="flex justify-between items-center my-5">
                         <div>
                             <label>
