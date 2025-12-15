15a956c5dd0d3e011bb202a8171be4d518084d59
rider <ridebus82@nate.com>
2025-12-15 12:07:56 +0900

    251215 asdlfjogogogogo

--- app/src/routes/(app)/+layout.svelte
+++ app/src/routes/(app)/+layout.svelte
@@ -88,9 +88,9 @@
                         }
                     }
                 }
+                console.log("bottomFixFormData!!!!!!!!!!!");
 
                 console.log(bottomFixFormData);
-                
             }
         }
     });
